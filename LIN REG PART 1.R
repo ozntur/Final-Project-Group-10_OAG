40c6cc63a9c093d15748304c071ade29ea84d6f4
Ayomideafolabi <aia0008@auburn.edu>
2020-11-19 05:52:31 -0600

    fully completed..confidence interval fixed

--- LIN REG PART 1.R
+++ LIN REG PART 1.R
@@ -1,33 +1,40 @@
 Estimates <- function(Response, Predictors,alpha){
    n <-  length(Response)   
-   df <-  n - dim(Predictors)[2]  #Degree of freedom
+   df <-  n - (dim(Predictors)[2]+1)  #Degree of freedom i.e no of observation minus no of parameter
    
   
-  Response <-  as.vector(Response)
-  Predictors <- as.matrix(Predictors)
+  Response <-  as.vector(Response)    #Response values  
+  Predictors <- as.matrix(Predictors) # Predictor values
   intercept <- rep(1, n)
   Predictors <- cbind(intercept,Predictors)
   Betas <- solve(t(Predictors)%*%Predictors)%*%t(Predictors)%*%Response
-  #colnames(Betas) <- "Coefficients"
   Betas
   
   
-   # Degree of freedom
-  
+  # Residual computation
   Predicted_Response <- Predictors%*%as.matrix(Betas)  # Predicted response
   Residuals <-  Response - Predicted_Response 
   
   
-  # Computed residuals
+  # Standard Error Computation
   Sigma.hat <- (1/df)*t(Residuals)%*%Residuals
   Sigma.hat <- as.vector(Sigma.hat)
-  Variance_Beta <-  Sigma.hat*(solve(t(Predictors)%*%Predictors))  #Variance of Betas
+  Sigma.hat
+  Variance_Beta <-  Sigma.hat*diag((solve(t(Predictors)%*%Predictors))) #Variance of Betas
+  SE <- sqrt( Variance_Beta)
  
- 
+
   #Confidence Interval 
  quant <- 1 - (alpha/2)
- CI_Beta <- c( Betas - qnorm(p=quant)*sqrt( Variance_Beta), Betas + qnorm(p=quant)*sqrt(Variance_Beta))
+ Lower_CI_Beta <- Betas - (qnorm(p=quant)*SE)
+ Upper_CI_Beta <- Betas + (qnorm(p=quant)*SE)
+ 
+ Summary <- data.frame("Coefficients Estimate" =  Betas, "Standard Error"=SE, "Lower Confidence Interval"=Lower_CI_Beta,
+                       "Upper Confidence Interval"=Upper_CI_Beta)
+ print(Summary)
+
  }
+
 
 
 
