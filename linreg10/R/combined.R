ac7b78e7dc457174bb495428550f0dfbfa236bb0
ozntur <ozan.turkes@gmail.com>
2020-11-20 23:34:55 -0600

    combining the functions into a single function

--- linreg10/R/combined.R
+++ linreg10/R/combined.R
@@ -3,17 +3,21 @@
 # y response
 # estimate beta
 
-##### ESTIMATE BETA #####
-
 exvals <- data.frame(y = c(runif(5000, 40,50)), 
-                            x1 = c(runif(5000, min = 0, max = 20)),
-                            x2 = (runif(20, 0,40)), 
-                            x3 = runif(20,0,50))
+                     x1 = c(runif(5000, min = 0, max = 20)),
+                     x2 = (runif(20, 0,40)), 
+                     x3 = runif(20,0,50))
 
 response = exvals$y
-covariates = exvals$x1
+# covariates = exvals$x1
+covariates = exvals[2:4]
 
-lm_func = function(response, covariates, alpha = 0.05) {
+##### ESTIMATE BETA  - Confidence Intervals #####
+
+# approach can be ; asymptotic or bootstrap
+
+lm_func = function(response, covariates, alpha = 0.05, approach = "asymptotic", 
+                   only_plot = "FALSE", pl_type = "hist") {
   
   # Make sure data formats are appropriate
   response <- as.vector(response)
@@ -24,30 +28,95 @@ lm_func = function(response, covariates, alpha = 0.05) {
   p <- dim(covariates)[2]
   df <- n - p
   
-  # Estimate beta through Eq. (6.1)
-  beta.hat <- solve(t(covariates)%*%covariates)%*%t(covariates)%*%response
+  ### CONFIDENCE INTERAVAL
   
-  # Estimate of the residual variance (sigma2) from Eq. (6.3)
-  # Compute residuals
-  resid <- response - covariates%*%as.matrix(beta.hat) 
-  sigma2.hat <- (1/df)*t(resid)%*%resid
+  if(approach == "asymptotic") {
+    # Estimate beta through Eq. (6.1)
+    beta.hat <- solve(t(covariates)%*%covariates)%*%t(covariates)%*%response
+    
+    # Estimate of the residual variance (sigma2) from Eq. (6.3)
+    # fitted values y_hat
+    fitted_vals <- covariates%*%as.matrix(beta.hat)
+    
+    # Compute residuals
+    resid <- response - fitted_vals
+    sigma2.hat <- (1/df)*t(resid)%*%resid
+    
+    # Estimate of the variance of the estimated beta from Eq. (6.2)
+    var.beta <- as.vector(sigma2.hat)*diag(solve(t(covariates)%*%covariates))
+    
+    # Estimate of the confidence interval based on alpha
+    quant <- 1 - alpha/2
+    ci.beta <- data.frame(lowerCI = beta.hat - qnorm(p = quant)*sqrt(var.beta), 
+                          upperCI = beta.hat + qnorm(p = quant)*sqrt(var.beta))
+  } else if(approach == "bootstrap") {
+    
+    print("still working on it")
+  } else {
+      print("apprach error")
+    }
   
-  # Estimate of the variance of the estimated beta from Eq. (6.2)
-  var.beta <- sigma2.hat*solve(t(covariates)%*%covariates)
+  ###### PLOT IF NECESSARY
   
-  # Estimate of the confidence interval based on alpha
-  quant <- 1 - alpha/2
-  ci.beta <- c(beta.hat - qnorm(p = quant)*sqrt(var.beta), beta.hat + 
-                 qnorm(p = quant)*sqrt(var.beta))
+  if(only_plot == TRUE) { 
+    pl_dat = data.frame(Residuals = resid, Fitted.Values = fitted_vals)
+    
+    if(pl_type == "res_fit") {
+      # 1. Residuals vs fitted-values
+      p <- ggplot(pl_dat, aes(y = Residuals, x = Fitted.Values )) + 
+        geom_point() + geom_smooth(method = lm, se = FALSE)
+      
+    } else if(pl_type == "qq") {
+      # 2. qq-plot of residuals
+      p <- ggplot(pl_dat, aes(sample = Residuals)) + stat_qq() + stat_qq_line()
+      
+    } else if(pl_type == "hist") {
+      #3. Histogram (or density) of residuals
+      p <- ggplot(pl_dat, aes(x = Residuals)) + geom_histogram(color="black", fill="white")
+      
+    } else {
+      print("error")
+    }
+    print(p)
+    return()
+  }
   
-  # fitted values y_hat
-  fitted_vals <- covariates%*%as.matrix(beta.hat) 
+  
+  ########## Mean Square Prediction Error (MSPE) computed in matrix form ######
+  
+  # n: number of observations in the data (number of rows)
+  n_mspe = as.matrix(1:length(response))
+  
+  # lm_func(response, covariates, alpha = 0.05)$residuals <- y_hat
+  #y_hat = resid
+  mspe <- (1/n_mspe) * resid**2
+
+  ##### F-test ######
+  n_f <- length(response)
+  p_f <- dim(covariates)[2]
+  df_f <- n - p
+
+  SSM = (resid - mean(response))**2
+  SSE = (response - resid)**2
+  
+  DFM = p - 1 # df1 , numerator
+  DFE = n-p # df2, denominator
+  
+  MSM = SSM / DFM
+  MSE = SSE / DFE
+  
+  f_star = MSM/MSE
+  p_value = pf(f_star, df1 = DFM, df2 = DFE)
   
   # Return all estimated values
   return(list(beta = beta.hat, sigma2 = sigma2.hat, 
               variance_beta = var.beta, ci = ci.beta, 
-              residuals = resid, fitted_vals = fitted_vals))
+              residuals = resid, fitted_vals = fitted_vals, 
+              mspe_error = mspe, p-value = p_value))
+  
 }
+
+lm_func(response = exvals$y, covariates = exvals[2:4], only_plot = TRUE, pl_type = "qq")
 
 
 
@@ -55,10 +124,10 @@ lm_func = function(response, covariates, alpha = 0.05) {
 ##### To test! ###########
 
 # Linear regression with lm() function
-fit_lm = lm(exvals$y~exvals$x1 -1)
+fit_lm = lm(y ~ x1 + x2 + x3 -1, data = exvals)
 
 # Linear regression with my_lm() function
-fit_my_lm = lm_func(exvals$y, exvals$x1)
+fit_my_lm = lm_func(exvals$y, exvals[2:4])
 
 # Compare outputs
 manual_results = c(fit_my_lm$beta, fit_my_lm$sigma2)
@@ -68,41 +137,6 @@ results = cbind(manual_results, base_results)
 row.names(results) = c("Beta", "Sigma")
 results
 
-
-plot()
-
-
-##### PLOTS ##### 
-library(ggplot2)
-
-# pl_type = c(res_fit, qq, hist)
-plot_lm <- function(response, covariates, pl_type) { 
-  
-  pl_dat = data.frame(Residuals = lm_func(response, covariates, alpha = 0.05)$residuals,
-                      Fitted.Values = lm_func(response, covariates, alpha = 0.05)$fitted_vals
-                      )
-  
-  if(pl_type == "res_fit") {
-  # 1. Residuals vs fitted-values
-  ggplot(pl_dat, aes(y = Residuals, x = Fitted.Values ))
-  p_1 + geom_point() + geom_smooth(method = lm, se = FALSE)
-  
-  } else if(pl_type == "qq") {
-  # 2. qq-plot of residuals
-  p_2 <- ggplot(pl_dat, aes(sample = Residuals))
-  p_2 + stat_qq() + stat_qq_line()
-  
-  } else if(pl_type == "hist") {
-  #3. Histogram (or density) of residuals
-  ggplot(pl_dat, aes(x = Residuals)) + geom_histogram(color="black", fill="white")
-    
-  } else {
-    print("error")
-  }
-}
-
-# TEST PLOTS
-plot_lm(response, covariates, "qq")
 
 ##### Mean Square Prediction Error (MSPE) computed in matrix form #####
 
@@ -141,24 +175,6 @@ ftest_lm <- function(response, covariates) {
   f_star = MSM/MSE
   p_value = pf(f_star, df1 = DFM, df2 = DFE)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
