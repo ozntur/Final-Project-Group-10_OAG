e78d8291a58b6623e0790ca47e9ad63e4270bb16
GEETA-KHAREL <kharelg44@gmail.com>
2020-11-23 22:49:55 -0600

    Updated f-test function

--- linreg10/R/ftest_fun.R
+++ linreg10/R/ftest_fun.R
@@ -1,23 +1,36 @@
 # F-test: compute the statistic in matrix form and output the
 # corresponding p-value. (see the formula in the project.html file)
-library(roxygen2)
-n<- 100
-y <-  as.vector(1:n)
-ybar<- (sum(y)/n)
-yhat<- 
-SSM<- matrix(NA,length(n),n)
-for (i in 1:n){
-  sum((yhat - ybar)^2))
-}
 
-SSE<- matrix(NA,length(n),n)
-for (i in 1:n){
-  sum((y - yhat)^2))
-}
+# x explanatory variable
+# y response
+set.seed(321)
+exvals <- data.frame(y = c(runif(6, 40,50)), 
+                     x1 = c(runif(6, min = 0, max = 20)),
+                     x2 = (runif(6, 0,40)), 
+                     x3 = runif(6,0,50))
 
-p<- 1
-DFM<- p-1
-DFE<- n-p
-MSM=SSM/DFM
-MSE=SSE/DFE
-Fstar<- MSM/MSE
+response = exvals$y
+# covariates = exvals$x1
+covariates = exvals[2:4]
+response <- as.vector(response)
+covariates <- as.matrix(covariates)
+
+beta.hat <- solve(t(covariates)%*%covariates)%*%t(covariates)%*%response
+
+fitted_vals <- covariates%*%as.matrix(beta.hat)
+
+n_f <- length(response)
+p_f <- dim(covariates)[2]
+
+SSM = colSums( (fitted_vals - mean(response))**2 )
+SSE = colSums( (response - fitted_vals)**2 )
+
+DFM = p_f - 1 # df1, numerator
+DFE = n_f - p_f # df2, denominator
+
+MSM = SSM / DFM
+MSE = SSE / DFE
+
+f_star = MSM/MSE
+
+p_value = pf(f_star, df1 = DFM, df2 = DFE)
