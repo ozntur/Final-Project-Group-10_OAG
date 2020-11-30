29344a68d701e4f4034c91ba53c553d624c976e1
Ayomideafolabi <aia0008@auburn.edu>
2020-11-29 18:46:06 -0600

    update on documentation

--- linreg10/R/Beta_function.R
+++ linreg10/R/Beta_function.R
@@ -1,20 +1,32 @@
+#' @title Linear Regression Coefficients Estimate
+#' @description Estimate the coefficient vector
+#' @param Response A \code{data frame} contains the response in the data
+#' region of interest, i.e. [a, b].
+#' @param Predictors A \code{data frame} contains the different type of predictors
+#' @return A \code{data frame} containing the following attributes:
+#' \describe{
+#'      \item{coefficient}{Estimated value of the coefficients}
+#'      }
+#' @author c(person("Ayomide","Afolabi"),person("Ozan","Turkes"),person("Geeta","Kharel"))
+#' @importFrom print
+#' @export
+#' @examples
+#' coeff(Response, Predictors)
+
 coeff <- function(Response, Predictors){
   n <-  length(Response)
   p <-  (dim(Predictors)[2]+1)
   df <-  n - p  #Degree of freedom i.e no of observation minus no of parameter
-  
+
   #Method 1
-  Response1 <-  as.vector(Response)   #Response values  
+  Response1 <-  as.vector(Response)   #Response values
   Predictors <- as.matrix(Predictors) # Predictor values
   intercept <- rep(1, n)
   Predictors1 <- cbind(intercept,Predictors)
   Betas1 <- solve(t(Predictors1)%*%Predictors1)%*%t(Predictors1)%*%Response1
-  print(data.frame("Coefficients.Estimate" = Betas1))
+  coefficient <- data.frame("Coefficients.Estimate" = Betas1
+  print(coefficient)
 }
 
-library(MASS)
-View(Boston)
-Response <- Boston$medv
-Predictors <- Boston[1:13]
 
-coeff(Response, Predictors)
+
